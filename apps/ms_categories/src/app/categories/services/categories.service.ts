1da4bbd4e19f637030f9eca202e0767a11051f5d
Alberto Domingo Basabe Acurero <alberto_12ba@hotmail.es>
2025-08-06 10:54:02 -0400

    add module categories

---
+++ apps/ms_categories/src/app/categories/services/categories.service.ts
@@ -0,0 +1,21 @@
+
+export class CategoriesService {
+
+
+  create(dto:any){
+    return 'created'
+  }
+
+  find(){
+    return []
+  }
+  findById(id:string){
+    return 'product1'
+  }
+
+  update(id:string,dto:Partial<any>){
+    return 'categorie updated'
+  }
+
+  
+}
