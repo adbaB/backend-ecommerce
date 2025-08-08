4251f2a0cdbf0f8bc9809fe7b749e08aa3dd5c68
Alberto Domingo Basabe Acurero <alberto_12ba@hotmail.es>
2025-08-08 14:42:21 -0400

    add database module

---
+++ apps/ms_categories/src/app/databases/database.module.ts
@@ -0,0 +1,21 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from "../categories/entities/categories.entity";
+
+@Module({imports:[TypeOrmModule.forRootAsync({
+  imports: [ConfigModule],
+  useFactory: (configService: ConfigService) => ({
+    type: 'postgres',
+    host: configService.get('DB_HOST'),
+    port: +configService.get('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_DATABASE'),
+    entities: [Category],
+    synchronize: true,
+    autoLoadEntities:true,
+  }),
+  inject: [ConfigService],
+})]})
+export class DatabaseModule {}
